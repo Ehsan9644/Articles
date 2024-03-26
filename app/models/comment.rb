5139edf1a23536065207c2fa19c78602d0b99830
ehsan <ehsanulhaq9644@gmail.com>
2024-03-26 15:45:40 +0500

    my first project

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ApplicationRecord
+  include Visible
+
+  belongs_to :article
+
+  after_create :status_change, if: ->{self.article.status != 'public'}
+
+  private
+  def status_change
+   
+    self.article.update(status:'public') 
+  end
+end
