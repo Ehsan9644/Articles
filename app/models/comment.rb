99c1e7c105ebad7c2aa349a5839d0ea74e657ba2
ehsan <ehsanulhaq9644@gmail.com>
2024-03-27 12:52:37 +0500

    done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,15 +4,11 @@ class Comment < ApplicationRecord
   belongs_to :article
 
   after_create :status_change, if: ->{self.article.status != 'public'}
-after_save :done, if: ->{self.article.exist? }
   private
   def status_change
    
     self.article.update(status:'public') 
   end
   private
-  def done
-   
-    puts "Article saved"
-  end
+  
 end
